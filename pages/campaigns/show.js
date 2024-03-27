207cb9e1e6254420f7d05481bb9a98ef2ec211a9
ras-24 <siregar.riyando@gmail.com>
2024-03-27 15:54:45 +0800

    Added Spinners and Error Handlers for Contribute

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -55,7 +55,7 @@ class CampaignShow extends Component {
       },
       {
         header: web3.utils.fromWei(balance, 'ether'),
-        meta: 'Campaign Balance (ethere)',
+        meta: 'Campaign Balance (ether)',
         description: 'The balance is how much money this campaign has left to spend.'
       }
     ];
