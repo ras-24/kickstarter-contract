c7c5ac5ac21c5d18955f794fea22e01fa9a61a46
ras-24 <siregar.riyando@gmail.com>
2024-03-28 16:35:18 +0800

    Finalizing Requests

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -3,13 +3,15 @@ import { Form, Button, Message, Input } from "semantic-ui-react";
 import Campaign from "../../../ethereum/campaign";
 import web3 from "../../../ethereum/web3";
 import { Link, Router } from "../../../routes";
-import Layout from '../../../components/Layout';
+import Layout from "../../../components/Layout";
 
 class RequestNew extends Component {
   state = {
     value: "",
     description: "",
     recipient: "",
+    loading: false,
+    errorMessage: "",
   };
 
   static async getInitialProps(props) {
@@ -18,11 +20,38 @@ class RequestNew extends Component {
     return { address };
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+    const { description, value, recipient } = this.state;
+
+    this.setState({ loading: true, errorMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods
+        .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
+        .send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
+          <a>
+            Back
+          </a>
+        </Link>
         <h3>Create a Request</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -48,7 +77,11 @@ class RequestNew extends Component {
               }
             />
           </Form.Field>
-          <Button primary>Create!</Button>
+
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
+            Create!
+          </Button>
         </Form>
       </Layout>
     );
